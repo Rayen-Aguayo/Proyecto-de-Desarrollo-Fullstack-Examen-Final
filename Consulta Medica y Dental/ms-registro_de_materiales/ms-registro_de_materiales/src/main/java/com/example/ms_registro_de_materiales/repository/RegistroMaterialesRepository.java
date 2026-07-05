9247e96edb58ff11c9424f891722ea39fd34e1a1
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-07-04 21:26:50 -0400

    feat:update ms-registro_de_materiales

---
+++ Consulta Medica y Dental/ms-registro_de_materiales/ms-registro_de_materiales/src/main/java/com/example/ms_registro_de_materiales/repository/RegistroMaterialesRepository.java
@@ -0,0 +1,9 @@
+package com.example.ms_registro_de_materiales.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.ms_registro_de_materiales.model.RegistroMateriales;
+
+public interface RegistroMaterialesRepository extends JpaRepository<RegistroMateriales, Long>{
+
+}
